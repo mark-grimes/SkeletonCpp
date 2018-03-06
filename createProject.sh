e5aeddde02bb435cf2f54c17662de009263f35d5
Mark Grimes <mark.grimes@rymapt.com>
2018-03-06 10:09:50 +0000

    Fix createProject.sh to also work on Mac OS X

--- createProject.sh
+++ createProject.sh
@@ -11,25 +11,34 @@ if [ $# -ne 1 ]; then
 	exit
 fi
 
+# Need to figure out if the sed version needs arguments changed. Sed on OS X is quite different.
+if sed -i -e s:"duMmy":"duMmy": -e s:"duMmy":"duMmy": CMakeLists.txt 2>/dev/null; then
+	# Normal version of sed
+	INPLACE='-i'
+else
+	# Probably Mac OS X
+	INPLACE='-i ""'
+fi
+
 REPLACEME_PROJECT_NAME="$1"
-REPLACEME_PROJECT_NAMESPACE=${REPLACEME_PROJECT_NAME,,} # Convert project name to lower case
+REPLACEME_PROJECT_NAMESPACE=`echo ${REPLACEME_PROJECT_NAME} | tr '[:upper:]' '[:lower:]'`  # Convert project name to lower case
 REPLACEME_PROJECT_TESTS_NAMESPACE="${REPLACEME_PROJECT_NAMESPACE}tests"
 
 mv "include/REPLACEME_PROJECT_NAMESPACE" "include/${REPLACEME_PROJECT_NAMESPACE}"
 mv "tests/include/REPLACEME_PROJECT_TESTS_NAMESPACE" "tests/include/${REPLACEME_PROJECT_TESTS_NAMESPACE}"
-sed -i -e s:"REPLACEME_PROJECT_NAMESPACE":"${REPLACEME_PROJECT_NAMESPACE}":g \
-       -e s:"REPLACEME_PROJECT_NAME":"${REPLACEME_PROJECT_NAME}":g \
-       -e s:"REPLACEME_PROJECT_TESTS_NAMESPACE":"${REPLACEME_PROJECT_TESTS_NAMESPACE}":g \
-           "tests/include/${REPLACEME_PROJECT_TESTS_NAMESPACE}/testinputs.h" \
-           "tests/src/testinputs.cpp.in" \
-           "include/${REPLACEME_PROJECT_NAMESPACE}/version.h" \
-           "src/version.cpp.in" \
-           "tests/src/test_version.cpp" \
-           "include/${REPLACEME_PROJECT_NAMESPACE}/SomeClass.h" \
-           "src/SomeClass.cpp" \
-           "tests/src/test_SomeClass.cpp" \
-           "include/${REPLACEME_PROJECT_NAMESPACE}/detail/TLSHandler.h" \
-           "src/TLSHandler.cpp" \
-           "tests/src/test_TLSHandler.cpp" \
-           "src/main.cpp" \
-           "CMakeLists.txt"
+sed $INPLACE -e s:"REPLACEME_PROJECT_NAMESPACE":"${REPLACEME_PROJECT_NAMESPACE}":g \
+             -e s:"REPLACEME_PROJECT_NAME":"${REPLACEME_PROJECT_NAME}":g \
+             -e s:"REPLACEME_PROJECT_TESTS_NAMESPACE":"${REPLACEME_PROJECT_TESTS_NAMESPACE}":g \
+                 "tests/include/${REPLACEME_PROJECT_TESTS_NAMESPACE}/testinputs.h" \
+                 "tests/src/testinputs.cpp.in" \
+                 "include/${REPLACEME_PROJECT_NAMESPACE}/version.h" \
+                 "src/version.cpp.in" \
+                 "tests/src/test_version.cpp" \
+                 "include/${REPLACEME_PROJECT_NAMESPACE}/SomeClass.h" \
+                 "src/SomeClass.cpp" \
+                 "tests/src/test_SomeClass.cpp" \
+                 "include/${REPLACEME_PROJECT_NAMESPACE}/detail/TLSHandler.h" \
+                 "src/TLSHandler.cpp" \
+                 "tests/src/test_TLSHandler.cpp" \
+                 "src/main.cpp" \
+                 "CMakeLists.txt"
