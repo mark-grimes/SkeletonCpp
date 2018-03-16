e55beedf584817d389bd198c4c49dc85ba2ac395
Mark Grimes <mark.grimes@rymapt.com>
2018-03-16 12:56:35 +0000

    Remove commented out code about secured websockets

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include "REPLACEME_PROJECT_NAMESPACE/SomeClass.h"
 #include "REPLACEME_PROJECT_NAMESPACE/version.h"
-#include "REPLACEME_PROJECT_NAMESPACE/detail/TLSHandler.h"
 #include <websocketpp/server.hpp>
-#include <websocketpp/config/asio.hpp>
-#include "REPLACEME_PROJECT_TESTS_NAMESPACE/testinputs.h"
+#include <websocketpp/config/asio_no_tls.hpp>
 
 int main( int argc, char* argv[] )
 {
@@ -13,17 +11,11 @@ int main( int argc, char* argv[] )
 	std::cout << "SomeClass::someValue returns '" << instance.someValue() << "'\n";
 
 	typedef websocketpp::server<websocketpp::config::asio> server_type;
-	typedef REPLACEME_PROJECT_NAMESPACE::detail::TLSHandler tls_handler_type;
-	typedef websocketpp::server<websocketpp::config::asio> connection_type;
 
 	try
 	{
 		int port=443;
 		server_type server;
-		// tls_handler_type tlsHandler( server.get_alog() );
-		// tlsHandler.setCertificateChainFile( REPLACEME_PROJECT_TESTS_NAMESPACE::testinputs::testFileDirectory+"tlscerts/serverA_cert.pem");
-		// tlsHandler.setPrivateKeyFile( REPLACEME_PROJECT_TESTS_NAMESPACE::testinputs::testFileDirectory+"tlscerts/serverA_key.pem");
-		// server.set_tls_init_handler( std::bind( &tls_handler_type::handle_tls_init_intermediate, &tlsHandler, std::placeholders::_1 ) );
 		server.set_http_handler(
 			[&server]( websocketpp::connection_hdl hdl )
 			{
@@ -52,7 +44,7 @@ int main( int argc, char* argv[] )
 				std::cout << "Connection open\n";
 			} );
 		server.set_message_handler(
-			[&server]( websocketpp::connection_hdl hdl, connection_type::message_ptr msg )
+			[&server]( websocketpp::connection_hdl hdl, server_type::message_ptr msg )
 			{
 				std::cout << "Got message '" << msg->get_payload() << "'\n";
 				server.get_con_from_hdl(hdl)->send( "Echo: "+msg->get_payload() );
